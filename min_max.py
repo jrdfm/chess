4faadb3f28fbabe5ffb3dbbb62f53efc658e1271
jrdfm <yaredfm@gmail.com>
2023-02-07 20:22:44 -0500

    fixed basic eval

--- min_max.py
+++ min_max.py
@@ -47,3 +47,38 @@ def evaluate_board(board):
 
     # print(value)
     return value
+
+def evaluate_board(board):
+    # Piece values
+    pawn_value = 1
+    knight_value = 3
+    bishop_value = 3
+    rook_value = 5
+    queen_value = 9
+    
+    # Mobility bonus
+    mobility_bonus = 0.1
+    
+    # Center control bonus
+    center_control_bonus = 0.2
+    
+    # King safety penalty
+    king_safety_penalty = 0.2
+    
+    # Evaluate the position
+    value = 0
+    for color in (chess.WHITE, chess.BLACK):
+        for piece_type in (chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN):
+            piece_value = pawn_value if piece_type == chess.PAWN else knight_value if piece_type == chess.KNIGHT else bishop_value if piece_type == chess.BISHOP else rook_value if piece_type == chess.ROOK else queen_value
+            for piece in board.pieces(piece_type, color):
+                mobility = len(board.attacks(piece))
+                center_control = 1 if piece in chess.SquareSet(chess.BB_CENTER) else 2
+                value += (mobility * mobility_bonus + center_control_bonus * center_control) * (1 if color == chess.WHITE else -1) * piece_value
+    
+    # Evaluate king safety
+    king_attacks = len(board.attacks(board.king(chess.WHITE))) - len(board.attacks(board.king(chess.BLACK)))
+    value -= king_attacks * king_safety_penalty
+    
+    return value
+
+
